a8fd582a47bcc986bb856cb84458f1d56868bdf1
MOAZZAM ALI <mazzmk6628@gmail.com>
2022-04-10 14:45:35 +0500

    added model for meetup and created rest api for meetups

--- meetups/views.py
+++ meetups/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 
 
 def index(request):
-    meetups = [
-        {"title": "First meetup"},
-        {"title": "Second meetup"},
-        {"title": "Third meetup"},
-    ]
-    return render(request, 'meetups/index.html', {"meetups": meetups})
+    meet = Meetup.objects.all()
+    return render(request, 'meetups/index.html', {"meetups": meet})
+
+
+def meetup_details(request, meetup_slug):
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetups_details.html', {'meetup_found':True,"meetups": selected_meetup})
+    except Exception as exc:
+        return render(request, 'meetups/meetups_details.html', {'meetup_found':False, 'exc': exc})
+
